37058163fb71aa6faa7dc3a4ee08391e18e5981b
rena2302 <khoivu2302@gmail.com>
2024-07-26 15:08:34 +0700

    aligment

--- src/main/java/com/mycompany/project/PlayerScoreScene.java
+++ src/main/java/com/mycompany/project/PlayerScoreScene.java
@@ -3,6 +3,7 @@ package com.mycompany.project;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -28,24 +29,55 @@ public class PlayerScoreScene {
 
         TableColumn<PlayerScore, String> modeColumn = new TableColumn<>("Mode");
         modeColumn.setCellValueFactory(new PropertyValueFactory<>("mode"));
+        modeColumn.setPrefWidth(100);
+        modeColumn.setCellFactory(column -> new TableCell<PlayerScore, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(item);
+                setAlignment(javafx.geometry.Pos.CENTER);
+            }
+        });
 
         TableColumn<PlayerScore, String> timeColumn = new TableColumn<>("Time");
         timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+        timeColumn.setPrefWidth(100);
+        timeColumn.setCellFactory(column -> new TableCell<PlayerScore, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(item);
+                setAlignment(javafx.geometry.Pos.CENTER);
+            }
+        });
 
         TableColumn<PlayerScore, Integer> scoreColumn = new TableColumn<>("Score");
         scoreColumn.setCellValueFactory(new PropertyValueFactory<>("score"));
+        scoreColumn.setPrefWidth(100);
+        scoreColumn.setCellFactory(column -> new TableCell<PlayerScore, Integer>() {
+            @Override
+            protected void updateItem(Integer item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(item == null ? "" : item.toString());
+                setAlignment(javafx.geometry.Pos.CENTER);
+            }
+        });
 
         TableColumn<PlayerScore, java.sql.Date> dateCompletedColumn = new TableColumn<>("Date Completed");
         dateCompletedColumn.setCellValueFactory(new PropertyValueFactory<>("dateCompleted"));
-
-        modeColumn.setPrefWidth(100);
-        timeColumn.setPrefWidth(100);
-        scoreColumn.setPrefWidth(100);
         dateCompletedColumn.setPrefWidth(150);
+        dateCompletedColumn.setCellFactory(column -> new TableCell<PlayerScore, java.sql.Date>() {
+            @Override
+            protected void updateItem(java.sql.Date item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(item == null ? "" : item.toString());
+                setAlignment(javafx.geometry.Pos.CENTER);
+            }
+        });
 
         table.getColumns().addAll(modeColumn, timeColumn, scoreColumn, dateCompletedColumn);
-
     }
+
 
     private void loadPlayerScores(String userId) {
         List<PlayerScore> scores = MyConnection.getPlayerScores(userId);
