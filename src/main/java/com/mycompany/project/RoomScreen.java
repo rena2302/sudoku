d2294b9cebbccc550c9533410b68a440a7c4af4a
chicchic07 <163634204+chicchic07@users.noreply.github.com>
2024-07-28 00:09:40 +0700

    Update logo for game

--- src/main/java/com/mycompany/project/RoomScreen.java
+++ src/main/java/com/mycompany/project/RoomScreen.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -58,6 +59,25 @@ public class RoomScreen {
         gridPane.setHgap(20);
         gridPane.setVgap(20);
         gridPane.setPadding(new Insets(40));
+
+        // Thêm thuộc tính Hgrow và Vgrow để các thành phần tự động mở rộng
+        GridPane.setHgrow(btnCreateRoom, Priority.ALWAYS);
+        GridPane.setHgrow(btnJoinRoom, Priority.ALWAYS);
+        GridPane.setHgrow(txtServerAddress, Priority.ALWAYS);
+        GridPane.setHgrow(txtPort, Priority.ALWAYS);
+
+        // Đảm bảo rằng các thành phần UI có kích thước thay đổi cùng với cảnh
+        lblTitle.prefWidthProperty().bind(roomScene.widthProperty().multiply(0.5));
+        lblTitle.prefHeightProperty().bind(roomScene.heightProperty().multiply(0.15));
+
+        btnCreateRoom.prefWidthProperty().bind(roomScene.widthProperty().multiply(0.4));
+        btnCreateRoom.prefHeightProperty().bind(roomScene.heightProperty().multiply(0.1));
+        
+        btnJoinRoom.prefWidthProperty().bind(roomScene.widthProperty().multiply(0.4));
+        btnJoinRoom.prefHeightProperty().bind(roomScene.heightProperty().multiply(0.1));
+        
+        txtServerAddress.prefWidthProperty().bind(roomScene.widthProperty().multiply(0.4));
+        txtPort.prefWidthProperty().bind(roomScene.widthProperty().multiply(0.4));
         
         gridPane.add(lblTitle, 0, 0, 2, 1);
         gridPane.add(btnCreateRoom, 0, 1);
@@ -77,7 +97,7 @@ public class RoomScreen {
         vbox.setAlignment(Pos.CENTER);
         
         vbox.setStyle(
-            "-fx-background-image: url('/images10.gif');" +
+            "-fx-background-image: url('/images13.png');" +
             "-fx-background-size: cover;"
         );
         
